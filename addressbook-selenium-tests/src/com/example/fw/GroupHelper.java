53a8581e337bef3ca121ce76349072a72d01d3da
amkatya <amkatya08@gmail.com>
2017-01-31 17:57:46 +0300

    lesson 4 (15)

--- addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
+++ addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
@@ -14,36 +14,67 @@ public class GroupHelper extends HelperBase {
 		super(manager);
 	}
 
-	public void initGroupCreation() {
+	//public void initGroupCreation() {
+	//	click(By.name("new"));
+	//}
+	public GroupHelper initGroupCreation() {
 		click(By.name("new"));
+		return this;
 	}
 
-	public void fillGroupForm(GroupData group) {
-	    type(By.name("group_name"), group.name);
-	    type(By.name("group_header"), group.header);
-	    type(By.name("group_footer"), group.footer);
+	//public void fillGroupForm(GroupData group) {
+	//    type(By.name("group_name"), group.getName());
+	//    type(By.name("group_header"), group.getHeader());
+	//    type(By.name("group_footer"), group.getFooter());
+	//}
+	public GroupHelper fillGroupForm(GroupData group) {
+	    type(By.name("group_name"), group.getName());
+	    type(By.name("group_header"), group.getHeader());
+	    type(By.name("group_footer"), group.getFooter());
+		return this;
+		
 	}
 
-	public void submitGroupCreation() {
+	//public void submitGroupCreation() {
+	//	click(By.name("submit"));
+	//}
+	public GroupHelper submitGroupCreation() {
 		click(By.name("submit"));
+		return this;
+		
 	}
 
-	public void deleteGroup(int index) {
+	//public void deleteGroup(int index) {
+	//	selectGroupByIndex(index);
+	//	click(By.name("delete"));
+	//}
+	public GroupHelper deleteGroup(int index) {
 		selectGroupByIndex(index);
 		click(By.name("delete"));
+		return this;
+		
 	}
 
 	private void selectGroupByIndex(int index) {
 		click(By.xpath("//input[@name='selected[]'][" + (index+1) + "]"));
 	}
 
-	public void initGroupModification(int index) {
+	//public void initGroupModification(int index) {
+	//	selectGroupByIndex(index);
+	//	click(By.name("edit"));
+	//}
+	public GroupHelper initGroupModification(int index) {
 		selectGroupByIndex(index);
 		click(By.name("edit"));
+		return this;
 	}
 
-	public void submitGroupModification() {
-		click(By.name("update"));		
+	//public void submitGroupModification() {
+	//	click(By.name("update"));		
+	//}
+	public GroupHelper submitGroupModification() {
+		click(By.name("update"));
+		return this;
 	}
 	
 	public void returntoGroupPage() {
@@ -61,5 +92,18 @@ public class GroupHelper extends HelperBase {
 		}
 		return groups;
 	}
+	//lesson4:
+	//public List<GroupData> getGroups() {
+	//	List<GroupData> groups = new ArrayList<GroupData>();
+	//	List<WebElement> checkboxes = driver.findElements(By.name("selected[]"));
+	//	for (WebElement checkbox : checkboxes) {
+	//		String title = checkbox.getAttribute("title");
+	//		String name = title.substring("Select (".length(), title.length() - ")".length());
+	//		GroupData group = new GroupData().withName(name);
+	//		groups.add(group); CLICK ON "group" > CTRL+1 > Inline local variable
+	//	}
+	//	return groups;
+	//}
+	
 
 }
--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -19,11 +19,18 @@ public class GroupCreationTests extends TestBase {
     // save old state
     List<GroupData> oldList = app.getGroupHelper().getGroups();
     
-    // actions    
-    app.getGroupHelper().initGroupCreation();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returntoGroupPage();
+    //// actions    
+    //app.getGroupHelper().initGroupCreation();
+	//app.getGroupHelper().fillGroupForm(group);
+    //app.getGroupHelper().submitGroupCreation();
+    //app.getGroupHelper().returntoGroupPage();
+    
+    // actions //lesson4 ~15min
+    app.getGroupHelper()
+    .initGroupCreation()
+	.fillGroupForm(group)
+    .submitGroupCreation()
+    .returntoGroupPage();
     
     // save new state
     List<GroupData> newList = app.getGroupHelper().getGroups();
